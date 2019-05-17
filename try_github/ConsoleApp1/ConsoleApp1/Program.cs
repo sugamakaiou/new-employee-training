ce35fc6dc6c204219709ab775fc0c67e5d6a8c32
sugamakaiou <sugama@occ.co.jp>
2019-05-17 18:54:21 +0900

    update

---
+++ try_github/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("何かキーを押したら終了します");
+        }
+
+    }
+}
